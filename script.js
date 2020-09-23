4c4c559e6e2e7d5d734295d322f0033012dd0a54
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2020-09-22 21:16:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,24 @@
-function verificar() {
-    var data = new Date
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
 
-    if (fano.value.length = 0 || fano.value > ano) {
-        window.alert('[ERRO] Verifique os dados novamente.')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img = document.createElement('img') //criando uma tag 'img' (como em html).
-        img.setAttribute('id', 'foto') // adicionando atributos a tag, como um id neste caso.
-        
-        
 
-        if (fsex[0].checked) {
-            gênero = 'Homem'
+function contar () {
+    let init = document.getElementById('txti')
+    let fm = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-            if(idade >= 0 && idade < 18){
-                img.setAttribute('src', 'menino.png')
-            } //menino
-            if(idade >= 18 && idade < 40){
-                img.setAttribute('src', 'homem.png')
-            } //homem adulto
-            if(idade > 40){
-                img.setAttribute('src', 'senhor.png')
-            } //homem idoso
-        } else if (fsex[1].checked) {
-            gênero = 'Mulher'
-
-            if(idade >= 0 && idade < 18){
-                img.setAttribute('src', 'menina.png')
-            } //menina
-            if(idade >= 18 && idade < 40){
-                img.setAttribute('src', 'mulher.png')
-            } //mulher adulta
-            if(idade > 40){
-                img.setAttribute('src', 'senhora.png')
-            } //mulher idosa
-        }
-    
-        res.style.textAlign = 'center'
-        res.innerHTML = `${gênero} de ${idade} anos`
-        res.appendChild(img) // para encaixar a imagem abaixo do resultado descrito.
-    
+    if (init.value.length == 0 || fm.value.length == 0 || passo.value.length ==0 ) {
+        window.alert('[ERRO] Faltam dados, verifique novamente!')
     }
+
+    else {
+        res.innerHTML = 'Contando...'
+        let i = Number(init.value)
+        let f = Number(fm.value)
+        let p = Number(passo.value)
+
+        for(let c = 1; c <= f; c+=p){
+            res.innerHTML += `${c}`
+        }
+    }
+
 }

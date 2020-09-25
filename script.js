d2f3bb193fccec369b6482b79333c3738f8c9d53
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2020-09-25 17:28:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 
 
 function contar () {
-    let init = document.getElementById('txti')
-    let fm = document.getElementById('txtf')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (init.value.length == 0 || fm.value.length == 0 || passo.value.length ==0 ) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
         window.alert('[ERRO] Faltam dados, verifique novamente!')
-    }
-
-    else {
-        res.innerHTML = 'Contando...'
-        let i = Number(init.value)
-        let f = Number(fm.value)
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for(let c = 1; c <= f; c+=p){
-            res.innerHTML += `${c}`
-        }
-    }
+        if (i < f) {
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{F917}`
+            }       
 
+        } else {
+            for ( let c = i; c >= f; c -= p) {
+                    res.innerHTML += `${c} \u{1f449}`
+            }
+
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }  
 }

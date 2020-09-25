7e37f61144f4c9519f73dd74fe19ab0bcecb169e
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2020-09-25 19:02:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-
-function contar () {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
-        window.alert('[ERRO] Faltam dados, verifique novamente!')
+   
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        
     } else {
-        res.innerHTML = 'Contando: '
-        //conversão das variavéis de string à Number:
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)     
-        if(p <= 0){
-            window.alert('Passo inválido! Considerando passo 1')
-            p = 1
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML= '' //para resetar, evitando acumulo de resultados.
+        while (c <= 10) {
+            let item = document.createElement('option') //criando uma tag option
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item) //adicionando um elemento child
+            c++ 
         }
+    }
 
-        if (i < f) {
-            //contagem crescente
-            for(let c = i; c <= f; c += p){
-                res.innerHTML += `${c} \u{F917}`
-            }       
 
-        } else { //contagem descrecente 
-            for ( let c = i; c >= f; c -= p) {
-                    res.innerHTML += `${c} \u{1f449}`
-            }
-
-        }
-        res.innerHTML += `\u{1F3C1}`
-    }  
 }

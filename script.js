829e4c2e0fc41b45ea2bdcd5eb314df412579ecb
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2020-09-25 18:03:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ function contar () {
         window.alert('[ERRO] Faltam dados, verifique novamente!')
     } else {
         res.innerHTML = 'Contando: '
+        //conversão das variavéis de string à Number:
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.value)
+        let p = Number(passo.value)     
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
 
         if (i < f) {
+            //contagem crescente
             for(let c = i; c <= f; c += p){
                 res.innerHTML += `${c} \u{F917}`
             }       
 
-        } else {
+        } else { //contagem descrecente 
             for ( let c = i; c >= f; c -= p) {
                     res.innerHTML += `${c} \u{1f449}`
             }
